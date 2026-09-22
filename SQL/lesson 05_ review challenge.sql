1aac749a90e6d48c36298963bb0ff6a1104bf2c9
Alan Santos <alan11.santos22@gmail.com>
2026-09-22 23:25:47 +0200

    Lesson 05 Final Recap Challenge

--- SQL/lesson 05_ review challenge.sql
+++ SQL/lesson 05_ review challenge.sql
@@ -25,8 +25,47 @@ SET bonus =
             END
 WHERE employee_id IN (1,2,3,4,5);
 
+
+
+
+--Create table projects
+
+CREATE Table projects (
+project_id INT GENERATED ALWAYS AS IDENTITY (START WITH 101 INCREMENT BY 1) PRIMARY KEY,
+employee_id INT,
+FOREIGN KEY (employee_id)
+    REFERENCES employees(employee_id)
+
+);
+
+
+
+--Add values to the project table
+INSERT INTO projects (employee_id)
+values
+(1),
+(2),
+(2),
+(3),
+(5);
+
 */
 
-SELECT *
-FROM employees;
 
+SELECT 
+        employee,
+        department,
+        salary,
+        COALESCE (e.bonus, 0) AS Bonus
+FROM  employees e
+WHERE salary > (
+                        SELECT 
+                            AVG(salary)
+                        FROM employees i 
+                        WHERE e.department = i.department
+                    )
+AND EXISTS (SELECT 
+                    employee_id
+              FROM projects a
+              WHERE e.employee_id = a.employee_id
+                );
