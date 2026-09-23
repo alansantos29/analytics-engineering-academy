b2f3e5b42aa6711cae67c6bdbc3e36aaa8be1e12
Alan Santos <alan11.santos22@gmail.com>
2026-09-23 23:13:31 +0200

    CTE Challenges

---
+++ SQL/lesson 06_CTE Common Table Expressions.sql
@@ -0,0 +1,114 @@
+--Common Table Expressions
+--=============================================
+--CTE Of employess in the IT Department
+--============================================
+--only employees from the IT department.
+/*
+WITH it_employees AS(
+
+SELECT 
+        employee,
+        department,
+        salary
+FROM employees
+WHERE department = 'IT'
+)
+
+SELECT *
+FROM it_employees;
+*/
+
+
+
+--===============================================
+
+--CTE of First identify employees earning more
+-- than 70,000. Then, from those employees, 
+--return only employees from Finance.
+
+/*
+WITH high_salary_employees AS (
+    SELECT
+            employee,
+            department,
+            salary
+    FROM employees
+    WHERE salary >70000
+)
+
+SELECT *
+FROM high_salary_employees
+WHERE department = 'Finance';
+*/
+
+--=============================================
+--aggregation inside a CTE
+--Calculate the total salary for each department, 
+--and then return only departments whose total 
+--salary is greater than 150000.
+
+/*
+WITH department_totals AS(
+    SELECT
+        department,
+        Sum(salary) AS total
+
+    FROM employees
+    GROUP BY department
+)
+
+SELECT *
+FROM department_totals
+WHERE total >150000;
+*/
+
+--============================================
+--Multiple CTEs
+/*
+WITH department_totals AS(
+    SELECT
+        department,
+        Sum(salary) AS total
+
+    FROM employees
+    GROUP BY department
+),
+
+large_departments AS (
+    SELECT *
+
+    FROM department_totals
+    WHERE total >150000
+)
+
+SELECT *
+FROM large_departments;
+*/
+
+--=========================================
+--Calculate how many projects each employee has, 
+--then return only employees who have more than 
+--one project.
+
+WITH employee_projects AS (
+    SELECT
+        i.employee,
+        o.project_id
+
+    FROM employees i
+    JOIN projects o
+    ON i.employee_id = o.employee_id
+),
+
+employee_projects2 AS (
+    SELECT
+        employee,
+        count(project_id) AS Number_of_Projects
+    FROM employee_projects
+    GROUP BY employee
+)
+
+ 
+SELECT *
+FROM employee_projects2
+WHERE Number_of_Projects >1;
